7f15a63f8e69387c492b4b51f3fb8e82f84704c4
Rakesh Ghosh <rakeshacot@gmail.com>
2024-12-01 03:48:08 +0530

    init commit 2

--- src/test/java/com/crowdseats/notification_service/NotificationServiceApplicationTests.java
+++ src/test/java/com/crowdseats/notification_service/NotificationServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CsNotificationServiceApplicationTests {
+class NotificationServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
